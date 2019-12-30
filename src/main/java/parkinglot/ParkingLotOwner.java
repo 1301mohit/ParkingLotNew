c7b728958a1caec674041f31c9945a051a65811f
1301mohit <mohitkr1301@gmail.com>
2019-12-30 19:49:50 +0530

    Refactor code remove parking attandant class

--- src/main/java/parkinglot/ParkingLotOwner.java
+++ src/main/java/parkinglot/ParkingLotOwner.java
@@ -5,10 +5,4 @@ import java.util.Random;
 
 public class ParkingLotOwner extends ParkingLotInformation{
 
-    ParkingAttendent parkingAttendent = new ParkingAttendent();
-
-    public void park(Integer position, Object vehicle) throws ParkingLotException {
-        parkingAttendent.parkVehicle(position, vehicle);
-    }
-
 }
