9010eed3647c9f4d70b99a76bc926329f2c08b17
1301mohit <mohitkr1301@gmail.com>
2020-01-02 17:17:56 +0530

    Refactor test we pass one more argument position when call park vehicle method

--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class ParkingLotTest {
 
-    ParkingLotSystem parkingLotSystem;
+    ParkingLot parkingLot;
     Object vehicle = null;
     Object vehicle1 = null;
     Object vehicle2 = null;
@@ -21,18 +21,19 @@ public class ParkingLotTest {
         listOfObserver = new ArrayList<>();
         listOfObserver.add(new ParkingLotOwner());
         listOfObserver.add(new AirportSecurity());
-        parkingLotSystem = new ParkingLotSystem(2);
+        parkingLot = new ParkingLot(2);
         vehicle = new Object();
         vehicle1 = new Object();
         vehicle2 = new Object();
-        parkingLotSystem.generateOwner(listOfObserver);
+        parkingLot.generateOwner(listOfObserver);
+        parkingLot.getListOfUnoccupiedPosition();
     }
 
     @Test
     public void parkVehicle_ShouldReturnTrue() {
         try {
-            Boolean isPark = parkingLotSystem.parkVehicle(vehicle);
-            Boolean isPark1 = parkingLotSystem.parkVehicle(vehicle1);
+            Boolean isPark = parkingLot.parkVehicle(vehicle, 1);
+            Boolean isPark1 = parkingLot.parkVehicle(vehicle1, 2);
             Assert.assertTrue(isPark && isPark1);
         } catch (ParkingLotException e) {
             e.printStackTrace();
@@ -42,9 +43,9 @@ public class ParkingLotTest {
     @Test
     public void parkMoreThanCapacity_ShouldReturnException() {
         try {
-            Boolean isPark1 = parkingLotSystem.parkVehicle(vehicle);
-            Boolean isPark2 = parkingLotSystem.parkVehicle(vehicle1);
-            Boolean isPark3 = parkingLotSystem.parkVehicle(vehicle2);
+            Boolean isPark1 = parkingLot.parkVehicle(vehicle, 1);
+            Boolean isPark2 = parkingLot.parkVehicle(vehicle1, 2);
+            Boolean isPark3 = parkingLot.parkVehicle(vehicle2, 3);
         } catch (ParkingLotException e) {
             Assert.assertEquals("Parking is full", e.getMessage());
         }
@@ -53,8 +54,9 @@ public class ParkingLotTest {
     @Test
     public void unParkVehicle_ShouldReturnTrue() {
         try {
-            parkingLotSystem.parkVehicle(vehicle1);
-            Boolean isUnPark = parkingLotSystem.unParkVehicle(vehicle1);
+            parkingLot.parkVehicle(vehicle1, 1);
+            Boolean isUnPark = parkingLot.unParkVehicle(vehicle1);
+            parkingLot.parkVehicle(vehicle2,1);
             Assert.assertEquals(true, isUnPark);
         } catch (ParkingLotException e) {
             e.printStackTrace();
@@ -64,8 +66,8 @@ public class ParkingLotTest {
     @Test
     public void unParkVehicle_VehicleIsNotPresent_ShouldThrowException() {
         try {
-            parkingLotSystem.parkVehicle(vehicle1);
-            Boolean isUnPark = parkingLotSystem.unParkVehicle(vehicle);
+            parkingLot.parkVehicle(vehicle1, 1);
+            Boolean isUnPark = parkingLot.unParkVehicle(vehicle);
         } catch (ParkingLotException e) {
             Assert.assertEquals("Vehicle is not present", e.getMessage());
         }
@@ -74,7 +76,7 @@ public class ParkingLotTest {
     @Test
     public void unParkVehicle_IfNoVehiclePresent_ShouldThrowException() {
         try{
-            parkingLotSystem.unParkVehicle(vehicle);
+            parkingLot.unParkVehicle(vehicle);
         } catch(ParkingLotException e) {
             Assert.assertEquals("Parking is Empty", e.getMessage());
         }
@@ -83,9 +85,9 @@ public class ParkingLotTest {
     @Test
     public void informObserver_IsFull_ReturnException() {
         try {
-            parkingLotSystem.parkVehicle(vehicle);
-            parkingLotSystem.parkVehicle(vehicle1);
-            parkingLotSystem.parkVehicle(vehicle2);
+            parkingLot.parkVehicle(vehicle, 1);
+            parkingLot.parkVehicle(vehicle1, 2);
+            parkingLot.parkVehicle(vehicle2, 3);
         } catch (ParkingLotException e) {
             Assert.assertEquals("Parking is full", e.getMessage());
         }
@@ -94,21 +96,21 @@ public class ParkingLotTest {
     @Test
     public void WhenFull_UnparkVehicle_InformObserver_IsNotFull() {
         try {
-            parkingLotSystem.generateOwner(listOfObserver);
-            parkingLotSystem.parkVehicle(vehicle);
-            parkingLotSystem.parkVehicle(vehicle1);
-            parkingLotSystem.unParkVehicle(vehicle);
+            parkingLot.generateOwner(listOfObserver);
+            parkingLot.parkVehicle(vehicle, 1);
+            parkingLot.parkVehicle(vehicle1, 2);
+            parkingLot.unParkVehicle(vehicle);
             Assert.assertFalse(listOfObserver.get(0).isFull && listOfObserver.get(1).isFull);
         } catch (ParkingLotException e) {
         }
     }
 
     @Test
-    public void parkVehicle_mockingPosition_ReturnPositionSame() {
+    public void parkVehicleAtPosition_ReturnPositionSame() {
         try {
-            parkingLotSystem.parkVehicle(vehicle);
-            Integer position = parkingLotSystem.findPosition(vehicle);
-            Assert.assertEquals(this.parkingLotSystem.position, position);
+            parkingLot.parkVehicle(vehicle, 1);
+            int position = parkingLot.findPosition(vehicle);
+            Assert.assertEquals(1, position);
         } catch (ParkingLotException e) {
             e.printStackTrace();
         }
@@ -117,9 +119,9 @@ public class ParkingLotTest {
     @Test
     public void parkedVehicleDate_ReturnDateSame() {
         try {
-            parkingLotSystem.parkVehicle(vehicle);
-            Date date = parkingLotSystem.getDate(vehicle);
-            Assert.assertEquals(parkingLotSystem.date, date);
+            parkingLot.parkVehicle(vehicle, 1);
+            Date date = parkingLot.getDate(vehicle);
+            Assert.assertEquals(parkingLot.date, date);
         } catch (ParkingLotException e) {
             e.printStackTrace();
         }
